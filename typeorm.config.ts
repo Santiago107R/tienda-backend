4351af5427dca78d1b3ab76c0ac1efb71a9317e2
Santiago107R <santiagorobles107k@gmail.com>
2026-05-05 11:16:50 -0300

    ENVIROMENT

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -8,11 +8,11 @@ const stage = process.env.STAGE || 'dev';
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: stage === 'dev' ? 'localhost' : process.env.DB_HOST,
-    port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: stage === 'dev' ? 'localhost' : process.env.PGHOST,
+    port: process.env.PGPORT ? +process.env.DB_PORT : 5432,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
     logging: stage === 'dev',
     entities: [join(__dirname, 'src/**/entities/*.entity{.ts,.js}')],
     migrations: [join(__dirname, 'src/migrations/*{.ts,.js}')],
